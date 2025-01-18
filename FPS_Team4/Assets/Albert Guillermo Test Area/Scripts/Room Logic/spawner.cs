d4af7f00a8f9c15fb79012ae2c1f4224b7190f3c
Bokumetsu2 <AMGuillermo@student.fullsail.edu>
2025-01-17 21:14:28 -0500

    Keep spawning enemies

--- FPS_Team4/Assets/Albert Guillermo Test Area/Scripts/Room Logic/spawner.cs
+++ FPS_Team4/Assets/Albert Guillermo Test Area/Scripts/Room Logic/spawner.cs
@@ -10,6 +10,7 @@ public class spawner : MonoBehaviour
     [SerializeField] private Transform[] spawnPositions;
     [SerializeField] private float enemyHpModifier = 1.0f;
     [SerializeField] private float enemyDamageModifier = 1.0f;
+    [SerializeField] private float spawnRadius = 5.0f; // radius for random spawns
 
     int spawnCount;
     bool startSpawning;
@@ -38,15 +39,34 @@ public class spawner : MonoBehaviour
         yield return new WaitForSeconds(timeBetweenSpawns);
 
         int spawnIndex = Random.Range(0, spawnPositions.Length);
+        Vector3 randomPosition = GetRandomPosition(spawnPositions[spawnIndex].position, spawnRadius);
 
-        GameObject newEnemy = Instantiate(enemyPrefab, spawnPositions[spawnIndex].position, spawnPositions[spawnIndex].rotation);
+        GameObject newEnemy = Instantiate(enemyPrefab, randomPosition, spawnPositions[spawnIndex].rotation);
         EnemyBasic enemyBasic = newEnemy.GetComponent<EnemyBasic>();
 
         if(enemyBasic != null)
         {
             enemyBasic.ApplyModifiers(enemyHpModifier, enemyDamageModifier);
+            enemyBasic.OnDeath += OnEnemyDeath;
         }
         spawnCount++;
         isSpawning = false;
+    }
+
+    Vector3 GetRandomPosition(Vector3 center, float radius)
+    {
+        float angle = Random.Range(0f, Mathf.PI * 2);
+        float distance = Random.Range(0f, radius);
+        Vector3 randomPosition = center + new Vector3(Mathf.Cos(angle) * distance, 0, Mathf.Sin(angle) * distance);
+        return randomPosition;
+    }
+
+    void OnEnemyDeath(EnemyBasic enemy)
+    {
+        spawnCount--;
+        if (startSpawning && spawnCount < numToSpawn)
+        {
+            StartCoroutine(SpawnEnemies());
+        }
     }
 }
