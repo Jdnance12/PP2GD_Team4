28a7dfd8d4e36b714f13b27e2d1041fbed6c9bee
Bokumetsu2 <AMGuillermo@student.fullsail.edu>
2025-01-19 23:47:54 -0500

    exe fix

--- FPS_Team4/Assets/Scripts/Player/EMPWeapon.cs
+++ FPS_Team4/Assets/Scripts/Player/EMPWeapon.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 //using UnityEngine.InputSystem.XR.Haptics;
 
@@ -32,7 +32,7 @@ public class EMPWeapon : MonoBehaviour
     [SerializeField] public float maxDistance;
     [SerializeField] public float displayDuration;
 
-    [SerializeField] public AnimatorController playerAnim;
+    //[SerializeField] public AnimatorController playerAnim;
 
     // Start is called before the first frame update
     void Start()
